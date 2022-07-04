0191daae1de9c35b42b75a229a7843a6081720bd
Jan Lukas Matejka <janlukas@matejka-email.de>
2022-07-04 21:12:50 +0200

    added support for reporting only specific alert levels and updatet linux binary

--- main.py
+++ main.py
@@ -23,100 +23,102 @@ if exists(config['options']['alertspath']):  # Only do if logs exist
     with open(config['options']['alertspath'],
               encoding='windows-1252') as logfilejson:  # Windows encoding for windows logs necessary...
         for logline in logfilejson:
+            alertlevel = config['options']['alertlevel']
             jsonlog = json.loads(logline)
-            rule = str(jsonlog['rule']['sidid'])
-            agent = str(jsonlog['agent_name'])
-            logfile = str(jsonlog['logfile'])
-            sysloghost = config['options']['sysloghosts']
-            isSyslog = False
-            if checkIP(logfile):
-                isSyslog = True
-                agent = list(sysloghost.keys())[list(sysloghost.values()).index(logfile)]
-            comment = str(jsonlog['rule']['comment'])
-            decoder = ""
-            pattern_log = ""
-            index = ""
-            index_field = ""
-            sep = {39:None,91: None,93:None}
-            length = length + 1
-            condition = True
-            pattern = r'Host: ' + agent + ', Rule: ' + rule + ', Desc: ' + comment + ', Logfile: ' + logfile
-            logdetails = []
-            if not re.search(r"'decoder':", str(jsonlog)):  #if the logline has no decoder, it have none extra fields
-                decoder = "Unkown"
-            else:
-                decoder = str(jsonlog["decoder"])
-            for field in fields: #check if configured fields in logline, and append to logdetails if they exist
-                pattern_field = field
-                if re.search(re.compile(r"'" + field + r"':"), str(jsonlog)):
-                    index_field = [i for i, field in enumerate(fields) if
-                                   re.search(pattern_field, str(field), re.IGNORECASE)]
-                    if index_field:
-                        logdetails.append(str(field_text[int(index_field[0])]) + " " + str(jsonlog[field]))
-            if str(jsonlog).__contains__("windows") or decoder == "Unkown" or str(jsonlog).__contains__("Unknown problem somewhere in the system.") or str(jsonlog).__contains__("Non standard syslog message (size too large)."):# in special cases i want to append whole original log
-                logdetails.append(f"Full log: StartFullLog {jsonlog['full_log']} EndFullLog")
-            for log in logs:    # check if logline which should be inserted already exists. If the whole log line is appended, only an approximation is possible because of the different timestamp formats.
-                levenshtein = 0  # conditon true means = insert and false = no insertion, but count for logline
-                if str(log).__contains__("StartFullLog"):
-                    logfulllog = str(log).split("StartFullLog ", 1)[1].split(" EndFullLog", 1)[0]
-                    jsonlogfulllog = str(jsonlog['full_log'])
-                    new_metric = ratio(logfulllog, jsonlogfulllog)
-                    if (new_metric >= levenshtein):
-                        levenshtein = new_metric #levenshtein decimal betwenn 1=equal and 0=nothing equal
-                if logdetails:
-                    pattern_log = f"{pattern}, {str(logdetails).translate(sep)}"
-                if re.search(re.escape(pattern), str(log), re.IGNORECASE):
-                    if re.search(re.escape(pattern_log), str(log), re.IGNORECASE) or levenshtein > 0.93:
-                        condition = False
-                        break
+            if jsonlog['rule']['level'] >= alertlevel:
+                rule = str(jsonlog['rule']['sidid'])
+                agent = str(jsonlog['agent_name'])
+                logfile = str(jsonlog['logfile'])
+                sysloghost = config['options']['sysloghosts']
+                isSyslog = False
+                if checkIP(logfile):
+                    isSyslog = True
+                    agent = list(sysloghost.keys())[list(sysloghost.values()).index(logfile)]
+                comment = str(jsonlog['rule']['comment'])
+                decoder = ""
+                pattern_log = ""
+                index = ""
+                index_field = ""
+                sep = {39:None,91: None,93:None}
+                length = length + 1
+                condition = True
+                pattern = r'Host: ' + agent + ', Rule: ' + rule + ', Desc: ' + comment + ', Logfile: ' + logfile
+                logdetails = []
+                if not re.search(r"'decoder':", str(jsonlog)):  #if the logline has no decoder, it have none extra fields
+                    decoder = "Unkown"
+                else:
+                    decoder = str(jsonlog["decoder"])
+                for field in fields: #check if configured fields in logline, and append to logdetails if they exist
+                    pattern_field = field
+                    if re.search(re.compile(r"'" + field + r"':"), str(jsonlog)):
+                        index_field = [i for i, field in enumerate(fields) if
+                                       re.search(pattern_field, str(field), re.IGNORECASE)]
+                        if index_field:
+                            logdetails.append(str(field_text[int(index_field[0])]) + " " + str(jsonlog[field]))
+                if str(jsonlog).__contains__("windows") or decoder == "Unkown" or str(jsonlog).__contains__("Unknown problem somewhere in the system.") or str(jsonlog).__contains__("Non standard syslog message (size too large)."):# in special cases i want to append whole original log
+                    logdetails.append(f"Full log: StartFullLog {jsonlog['full_log']} EndFullLog")
+                for log in logs:    # check if logline which should be inserted already exists. If the whole log line is appended, only an approximation is possible because of the different timestamp formats.
+                    levenshtein = 0  # conditon true means = insert and false = no insertion, but count for logline
+                    if str(log).__contains__("StartFullLog"):
+                        logfulllog = str(log).split("StartFullLog ", 1)[1].split(" EndFullLog", 1)[0]
+                        jsonlogfulllog = str(jsonlog['full_log'])
+                        new_metric = ratio(logfulllog, jsonlogfulllog)
+                        if (new_metric >= levenshtein):
+                            levenshtein = new_metric #levenshtein decimal betwenn 1=equal and 0=nothing equal
+                    if logdetails:
+                        pattern_log = f"{pattern}, {str(logdetails).translate(sep)}"
+                    if re.search(re.escape(pattern), str(log), re.IGNORECASE):
+                        if re.search(re.escape(pattern_log), str(log), re.IGNORECASE) or levenshtein > 0.92:
+                            condition = False
+                            break
+                        else:
+                            condition = True
                     else:
                         condition = True
-                else:
-                    condition = True
-            if condition: #insertion of uniqe logline
-                if decoder.__contains__("syscheck_integrity"):
-                    logs.append(
-                        f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
-                        f"Desc: {jsonlog['rule']['comment']}, Path: {jsonlog['SyscheckFile']['path']}, Logfile: "
-                        f"{jsonlog['logfile']}, Decoder: {decoder}")
-                elif not logdetails:
-                    logs.append(
-                        f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
-                        f"Desc: {jsonlog['rule']['comment']}, Logfile: {jsonlog['logfile']}, Decoder: "
-                        f"{decoder}")
-                else:
-                    logs.append(
-                        f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
-                        f"Desc: {jsonlog['rule']['comment']}, Logfile: {jsonlog['logfile']}, {str(logdetails).translate(sep)}, Decoder: "
-                        f"{decoder}")
-            elif not condition: #  Suspression for duplicated logs. Appending "+ More" to processed logline, because logline alredy exists
-                fullloginlog = False
-                for detail in logdetails:
-                    if detail.__contains__("Full log:"):
-                        fullloginlog = True
-                        break
-                if fullloginlog:  # if full log line is appended wie use levenshtein to determine the equality.
-                    pattern_details = pattern + ", " + str(logdetails).translate(sep)
-                    index = [i for (i, log) in enumerate(logs) if (ratio(pattern_details, str(re.sub(r', Decoder: .*',"",str(re.sub(r'^Time(.*?), ', "", log))))) >= 0.93)]
-                    if index:
-                        temp = logs[int(index[0])]
-                        logs[int(index[0])] = f"{str(temp)} + More"
-                else:
-                    if logdetails:
-                        pattern = pattern + ", " + str(logdetails).translate(sep)
-                    index = [i for i, log in enumerate(logs) if re.search(re.escape(pattern), str(log), re.IGNORECASE)]
-                    if index:
-                        temp = logs[int(index[0])]
-                        logs[int(index[0])] = f"{str(temp)} + More"
+                if condition: #insertion of uniqe logline
+                    if decoder.__contains__("syscheck_integrity"):
+                        logs.append(
+                            f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
+                            f"Desc: {jsonlog['rule']['comment']}, Path: {jsonlog['SyscheckFile']['path']}, Logfile: "
+                            f"{jsonlog['logfile']}, Decoder: {decoder}")
+                    elif not logdetails:
+                        logs.append(
+                            f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
+                            f"Desc: {jsonlog['rule']['comment']}, Logfile: {jsonlog['logfile']}, Decoder: "
+                            f"{decoder}")
+                    else:
+                        logs.append(
+                            f"Time: {jsonlog['timestamp']}, Host: {agent}, Rule: {jsonlog['rule']['sidid']}, "
+                            f"Desc: {jsonlog['rule']['comment']}, Logfile: {jsonlog['logfile']}, {str(logdetails).translate(sep)}, Decoder: "
+                            f"{decoder}")
+                elif not condition: #  Suspression for duplicated logs. Appending "+ More" to processed logline, because logline alredy exists
+                    fullloginlog = False
+                    for detail in logdetails:
+                        if detail.__contains__("Full log:"):
+                            fullloginlog = True
+                            break
+                    if fullloginlog:  # if full log line is appended wie use levenshtein to determine the equality.
+                        pattern_details = pattern + ", " + str(logdetails).translate(sep)
+                        index = [i for (i, log) in enumerate(logs) if (ratio(pattern_details, str(re.sub(r', Decoder: .*',"",str(re.sub(r'^Time(.*?), ', "", log))))) >= 0.92)]
+                        if index:
+                            temp = logs[int(index[0])]
+                            logs[int(index[0])] = f"{str(temp)} + More"
+                    else:
+                        if logdetails:
+                            pattern = pattern + ", " + str(logdetails).translate(sep)
+                        index = [i for i, log in enumerate(logs) if re.search(re.escape(pattern), str(log), re.IGNORECASE)]
+                        if index:
+                            temp = logs[int(index[0])]
+                            logs[int(index[0])] = f"{str(temp)} + More"
 
-    for log in logs:   #count "+ Mores" and replace with counted number
-        temp = log
-        morecount = temp.count("+ More")
-        if morecount > 0:
-            logswnumber.append(
-                f"{str(log.replace(' + More', ''))} Duplicated messages suspressed! Total: {str(morecount + 1)}")
-        else:
-            logswnumber.append(str(log))
+        for log in logs:   #count "+ Mores" and replace with counted number
+            temp = log
+            morecount = temp.count("+ More")
+            if morecount > 0:
+                logswnumber.append(
+                    f"{str(log.replace(' + More', ''))} Duplicated messages suspressed! Total: {str(morecount + 1)}")
+            else:
+                logswnumber.append(str(log))
 
 # Print entrys for each host
 for host in hosts:
@@ -124,7 +126,7 @@ for host in hosts:
     printok = ""
     for log in logswnumber:
         if re.search(re.escape("Host: "+str(host)), str(log), re.IGNORECASE):
-            print(str(log).replace(' StartFullLog', '').replace('EndFullLog ', ''))
+            print(str(log).replace(' StartFullLog', '').replace('EndFullLog', ''))
             printok = True
     if not printok:
         print(f"OK. No alerts found for {host}")
@@ -135,7 +137,7 @@ for host in list(sysloghost.keys()):
     printok = ""
     for log in logswnumber:
         if re.search(re.escape("Host: "+str(host)), str(log), re.IGNORECASE):
-            print(str(log).replace(' StartFullLog', '').replace('EndFullLog ', ''))
+            print(str(log).replace(' StartFullLog', '').replace('EndFullLog', ''))
             printok = True
     if not printok:
         print(f"OK. No alerts found for {host}")
